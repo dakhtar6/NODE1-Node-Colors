e6876a7f07ed6392afbc969ac3c44d73393a6fff
dakhtar6 <daneyalakhtar@gmail.com>
2015-02-20 16:47:41 -0700

    completed exercise

---
+++ islight.js
@@ -0,0 +1,18 @@
+
+var r = process.argv[2];
+var g = process.argv[3];
+var b = process.argv[4];
+
+
+
+function islight (r, g, b) {
+	var luminosity = 0.2126*r + 0.7152*g + 0.0722*b;
+	if (luminosity > 155) {
+			return "light";
+		} else {
+			return "dark";
+}
+
+}
+
+console.log(islight(r, g, b));
---
+++ luminosity.js
@@ -0,0 +1,31 @@
+
+///////////
+//Part I//
+/////////
+
+var r = process.argv[2];
+var g = process.argv[3];
+var b = process.argv[4]; 
+
+var luminosity = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+console.log(luminosity); 
+
+
+////////////
+//Part II//
+//////////
+
+// var r = process.argv[2];
+// var g = process.argv[3];
+// var b = process.argv[4]; 
+
+// var luminosity = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+// if (luminosity > 155) {
+// 	return "light"; 
+// } else {
+// 	return "dark"; 
+// }
+
+// console.log(luminosity); 
